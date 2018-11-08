5fc57c0949ba55c6d165d2d51cfae8f77eaeee96
Victor Geraldsson <victor@geraldsson.org>
2018-11-09 00:20:27 +0100

    Add basic game rendering

--- src/game/view.c
+++ src/game/view.c
@@ -1,12 +1,8 @@
+#include <string.h>
 #include "tetmino.h"
 #include "view.h"
 
 #define MIN(a,b)	((a) < (b) ? (a) : (b))
-
-struct copyrect {
-	int width, height;
-	int di, si;	/* dest and source bitmap positions */
-};
 
 /* get intersection */
 static struct copyrect cliprect(int dw, int dh, int sw, int sh,
@@ -49,28 +45,77 @@ static void make_piece(unsigned char *piece, unsigned bits)
 	}
 }
 
-void render_tetmino(unsigned char *bmp, int w, int h, const struct tetmino *t, int mask)
+static void render_piece(const struct piece *p, int mask)
 {
-	struct copyrect r = cliprect(
+	unsigned char *bmp = p->bitmap.bitmap;
+	struct copyrect r = p->r;
+
+	for (; r.height > 0; r.height--) {
+		int i;
+		for (i=0; i < r.width; i++) {
+			if (p->piece[r.si + i])
+				bmp[r.di + i] = p->piece[r.si + i] & mask;
+		}
+		r.di += p->bitmap.width;
+		r.si += PIECE_WIDTH;
+	}
+}
+
+void render_tetmino_piece(struct piece *p, const struct tetmino *t, int mask)
+{
+	int w = p->bitmap.width;
+	int h = p->bitmap.height;
+
+	p->r = cliprect(
 		w, h, PIECE_WIDTH, PIECE_HEIGHT,
-		t->col - LEFT_WALL_WIDTH, h - PIECE_HEIGHT - t->row
+		t->col - LEFT_WALL_WIDTH, h - (PIECE_HEIGHT - 1) - t->row
 	);
 
-	if (r.width > 0 && r.height > 0) {
-		unsigned char piece[PIECE_WIDTH * PIECE_HEIGHT];
+	make_piece(p->piece, t->shape);
 
-		make_piece(piece, t->shape);
+	mask &= 0x88 | (0x11 * (t->piece + 1));
 
-		mask &= 0x88 | (0x11 * (t->piece + 1));
+	render_piece(p, mask);
+}
 
-		for (; r.height > 0; r.height--) {
-			int i;
-			for (i=0; i < r.width; i++) {
-				if (piece[r.si + i])
-					bmp[r.di + i] = piece[r.si + i] & mask;
-			}
-			r.di += w;
-			r.si += PIECE_WIDTH;
+static void render_blocks(struct blocks *b)
+{
+	int w = b->piece.bitmap.width;
+	int h = b->piece.bitmap.height;
+	unsigned char *back = b->piece.bitmap.bitmap;
+	unsigned char *front = b->piece.bitmap.bitmap + w * h;
+
+	if (!b->rendered) {
+		b->rendered = 1;
+		memcpy(front, back, w * h);
+		front = NULL;
+	}
+	drawtiles(front, back, w, h, b->x, b->y, &b->tiles);
+
+	render_piece(&b->piece, 0);
+}
+
+void render_tetmino_blocks(struct blocks *b, const struct tetmino *t, int mask)
+{
+	render_tetmino_piece(&b->piece, t, mask);
+
+	render_blocks(b);
+}
+
+void render_cleared_blocks(struct bitmap *bitmap, int row, blocks_row mask)
+{
+	unsigned char *bmp = bitmap->bitmap;
+	int w = bitmap->width;
+	int h = bitmap->height;
+
+	if (mask) {
+		int i = 0;
+		for (; mask; mask >>= 1, i++) {
+			if (mask & 1)
+				bmp[w * (h - row) + i] = 0;
 		}
+	} else {
+		memmove(bmp + w, bmp, w * (h - row));
+		memset(bmp , 0, w);
 	}
 }
--- src/game/view.h
+++ src/game/view.h
@@ -1,3 +1,31 @@
 /* Bitmap manipulation */
 
-void render_tetmino(unsigned char *bmp, int w, int h, const struct tetmino *t, int mask);
+#include "../terminal/textgfx.h"
+
+struct bitmap {
+	unsigned char *bitmap;
+	int width;	/* width in columns */
+	int height;	/* height in rows */
+};
+
+struct copyrect {
+	int width, height;
+	int di, si;	/* dest and source bitmap positions */
+};
+
+struct piece {
+	struct bitmap bitmap;
+	struct copyrect r;
+	unsigned char piece[PIECE_WIDTH * PIECE_HEIGHT];
+};
+
+struct blocks {
+	struct piece piece;
+	struct tg_tiles tiles;
+	int x, y;
+	int rendered;
+};
+
+void render_tetmino_piece(struct piece *, const struct tetmino *t, int mask);
+void render_tetmino_blocks(struct blocks *, const struct tetmino *t, int mask);
+void render_cleared_blocks(struct bitmap *, int row, blocks_row mask);
